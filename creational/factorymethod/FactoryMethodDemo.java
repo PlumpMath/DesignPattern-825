bfe27cb8fc07f7b01dc84f02bb32d5507b4b5e73
spino9330 <spino9330@gmail.com>
2016-01-30 11:16:09 +0100

    add creational.factorymethod

--- creational/factorymethod/FactoryMethodDemo.java
+++ creational/factorymethod/FactoryMethodDemo.java
@@ -1,7 +1,71 @@
-public class FactoryMethodDemo
-{
+public class FactoryMethodDemo{
+
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        ShapeFactory shapeFactory = new ShapeFactory();
+
+        //get an object of Circle and call its draw method.
+        Shape shape1 = shapeFactory.getShape("CIRCLE");
+        shape1.draw();
+
+        //get an object of Rectangle and call its draw method.
+        Shape shape2 = shapeFactory.getShape("RECTANGLE");
+        shape2.draw();
+
+        //get an object of Square and call its draw method.
+        Shape shape3 = shapeFactory.getShape("SQUARE");
+        shape3.draw();
+    }
+}
+
+//Shape interface
+interface Shape {
+   void draw();
+}
+
+//classes that implement Shape interface
+class Rectangle implements Shape{
+
+    @Override
+    public void draw(){
+
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
+
+class Circle implements Shape{
+
+    @Override
+    public void draw(){
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
+
+class Square implements Shape {
+
+    @Override
+    public void draw(){
+        System.out.println("Inside Square::draw() method.");
+    }
+}
+
+//Factory class
+class ShapeFactory {
+   //use getShape method to get object of type shape
+    public Shape getShape(String shapeType){
+        if(shapeType == null){
+            return null;
+        }
+        if(shapeType.equalsIgnoreCase("CIRCLE")){
+            return new Circle();
+
+        } else if(shapeType.equalsIgnoreCase("RECTANGLE")){
+            return new Rectangle();
+
+        } else if(shapeType.equalsIgnoreCase("SQUARE")){
+            return new Square();
+        }
+
+        return null;
     }
 }
--- creational/factorymethod/readme.md
+++ creational/factorymethod/readme.md
@@ -2,4 +2,4 @@
 
 Define an interface for creating an object, but let subclasses decide which class to instantiate.
 Hide creation logic to client and by a Factory class it create objects.
-There are entities that grant the division of responsability and code reusability  
+There are entities that grant the division of **responsability** and code **reusability**.
